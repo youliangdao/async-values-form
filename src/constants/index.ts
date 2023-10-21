1c50eec44390a38832c7f110ee40ff42078e467e
youliangdao <youliangdaojing5@gmail.com>
2023-10-21 21:44:49 +0900

    [add] Selectのoptionを取得するカスタムフックを定義＋初期値として設定するuserIdとalbumId、photoIdを定義

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const INITIAL_USER_ID = 1;
+export const INITIAL_ALBUM_ID = 1;
+export const INITIAL_PHOTO_ID = 1;
---
+++ src/hooks/useOptions.ts
@@ -0,0 +1,78 @@
+import { getGetAlbumsQueryKey, useGetAlbums } from "@/api/albums/albums";
+import { getGetPhotosQueryKey, useGetPhotos } from "@/api/photos/photos";
+import { getGetUsersQueryKey, useGetUsers } from "@/api/users/users";
+
+type Props = {
+  userId?: string;
+  albumId?: string;
+};
+
+type Option = {
+  value: string;
+  label: string;
+};
+
+export const useOptions = ({ userId, albumId }: Props) => {
+  const { data: userOptions } = useGetUsers<Option[]>({
+    query: {
+      queryKey: getGetUsersQueryKey(),
+      select: (data) =>
+        [
+          { value: "0", label: "ユーザーを選択してください" },
+          ...data.data.map((user) => ({
+            value: String(user.id),
+            label: user.username,
+          })),
+        ] as Option[],
+    },
+  });
+  const { data: albumOptions } = useGetAlbums<Option[]>({
+    query: {
+      queryKey: getGetAlbumsQueryKey(),
+      select: (data) => {
+        if (data.data.length === 0) {
+          return [{ value: "", label: "選択できるアルバムがありません" }];
+        }
+
+        return [
+          { value: "0", label: "アルバムを選択してください" },
+          ...data.data
+            .filter((album) => album.userId === Number(userId))
+            .map((album) => ({ value: String(album.id), label: album.title })),
+        ] as Option[];
+      },
+    },
+  });
+  const { data: photoOptions } = useGetPhotos<Option[]>({
+    query: {
+      queryKey: getGetPhotosQueryKey(),
+      select: (data) => {
+        if (data.data.length === 0) {
+          return [{ value: "", label: "選択できる写真がありません" }];
+        }
+
+        return [
+          { value: "0", label: "写真を選択してください" },
+          ...data.data
+            .filter((photo) => photo.albumId === Number(albumId))
+            .map((album) => ({
+              value: String(album.id),
+              label: album.title,
+            })),
+        ] as Option[];
+      },
+    },
+  });
+
+  return {
+    userOptions: userOptions || [
+      { value: "", label: "選択できるユーザーがいません" },
+    ],
+    albumOptions: albumOptions || [
+      { value: "", label: "選択できるアルバムがありません" },
+    ],
+    photoOptions: photoOptions || [
+      { value: "", label: "選択できる写真がありません" },
+    ],
+  };
+};
